eb2d90c20be559784d8896d6d689c37315600c85
Scott Chamberlain <myrmecocystus@gmail.com>
2014-06-07 00:34:15 -0700

    improved events fxn to take in droplet or event id

--- R/events.R
+++ R/events.R
@@ -1,14 +1,34 @@
 #' Get information on an event
 #'
 #' @export
-#' @param id Event ID.
+#' @param x Event ID or a droplet object.
 #' @template params
 #' @examples \dontrun{
-#' events(id=25914777)
+#' events(x=25914777)
+#' gg <- droplets_get() %>% droplets_power_on
+#' events(x=gg)
+#' 
+#' droplets_get() %>% droplets_power_off %>% events
 #' }
 
-events <- function(id=NULL, what="parsed", ...)
+events <- function(x=NULL, what="parsed", ...)
 {
+  id <- check_event(x)
   assert_that(!is.null(id))
+#   do_GET(what, TRUE, sprintf('events/%s', id))
   do_GET(what, TRUE, sprintf('events/%s', id), ...)
+}
+
+check_event <- function(x){
+  if(!is.null(x)){
+    if(is.list(x)){
+      if(is.null(x$event_id)) stop("No event id found")
+      x <- x$event_id
+      if(!is.numeric(x)) stop("Could not detect a event id")
+    } else {
+      x <- as.numeric(as.character(x))
+      if(!is.numeric(x)) stop("Could not detect an event id")
+    }
+    x 
+  } else { NULL }
 }
