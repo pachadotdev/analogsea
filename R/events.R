5c7ee52eabcf7a2694689aae98eaa513e464d069
Scott Chamberlain <myrmecocystus@gmail.com>
2014-06-07 09:46:51 -0700

    added dotdot to internal doGet call in events

--- R/events.R
+++ R/events.R
@@ -4,7 +4,7 @@
 #' @param x Event ID or a droplet object.
 #' @template params
 #' @examples \dontrun{
-#' events(x=25914777)
+#' events(x=26024501)
 #' gg <- droplets_get() %>% droplets_power_on
 #' events(x=gg)
 #' 
@@ -15,7 +15,6 @@ events <- function(x=NULL, what="parsed", ...)
 {
   id <- check_event(x)
   assert_that(!is.null(id))
-#   do_GET(what, TRUE, sprintf('events/%s', id))
   do_GET(what, TRUE, sprintf('events/%s', id), ...)
 }
 
