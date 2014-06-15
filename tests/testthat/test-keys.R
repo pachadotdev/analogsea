25baa2bde40ced2146eb134cff19dbfeaf678442
Scott Chamberlain <myrmecocystus@gmail.com>
2014-06-15 12:58:55 -0700

    fixed up keys test

--- tests/testthat/test-keys.R
+++ tests/testthat/test-keys.R
@@ -6,18 +6,22 @@ b <- keys(what = 'raw')
 c <- keys(ssh_key_id=89103)
 
 test_that("keys returns the correct dimensions", {
-  expect_equal(NCOL(a), 8)
-  expect_equal(names(a), c('id','name','slug','memory','cpu','disk','cost_per_hour','cost_per_month'))
+  expect_equal(length(a), 1)
+  expect_equal(names(a), 'ssh_keys')
 
   expect_equal(length(b), 8)
+  
+  expect_equal(length(c), 1)
 })
 
 test_that("keys returns the correct class", {
-  expect_is(a, "data.frame")
-  expect_is(a$cost_per_hour, "numeric")
+  expect_is(a, "list")
+  expect_is(a$ssh_keys[[1]]$id, "numeric")
   expect_is(b, "response")
+  expect_is(c, "list")
 })
 
 test_that("incorrect input to what param returns NULL", {
-  expect_null(keys("asfasd"))
+  expect_error(keys("asfasd"))
+  expect_error(keys(ssh_key_id=891032343))
 })
