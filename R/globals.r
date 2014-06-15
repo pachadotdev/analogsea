ec1c75afe10d644a98a7e47abcb167ccb7ef7285
Scott Chamberlain <myrmecocystus@gmail.com>
2014-06-15 15:52:02 -0700

    added id to globals list

--- R/globals.r
+++ R/globals.r
@@ -1,3 +1,3 @@
 if (base::getRversion() >= "2.15.1") {
-  utils::globalVariables(c("client_id","api_key","numsubjects","au"))
+  utils::globalVariables(c("client_id","api_key","numsubjects","au","id"))
 }
