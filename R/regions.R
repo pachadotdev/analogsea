0087208eaa66a9e123b29b2304092361ba5e764e
Scott Chamberlain <myrmecocystus@gmail.com>
2014-08-16 23:13:27 -0700

    added page and per_page params to regions and sizes fxns

--- R/regions.R
+++ R/regions.R
@@ -4,12 +4,15 @@
 #' @template params
 #' @examples \dontrun{
 #' regions()
+#' regions(per_page=2)
 #' regions('raw')
 #' }
 
-regions <- function(what="parsed", ...)
+regions <- function(what="parsed", page=1, per_page=25, ...)
 {
-  do_GET(what, path='regions', parse=if(what=='parsed') TRUE else FALSE, ...)
+  do_GET(what, path='regions',
+         query = ct(page=page, per_page=per_page), 
+         parse=if(what=='parsed') TRUE else FALSE, ...)
 #   if(what == 'raw'){ res } else {
 #     do.call(rbind, lapply(res$regions, data.frame, stringsAsFactors = FALSE))
 #   }
--- R/sizes.R
+++ R/sizes.R
@@ -5,14 +5,17 @@
 #' @return A data.frame with available sizes (RAM, disk, no. CPU's) and their costs
 #' @examples \dontrun{
 #' sizes()
+#' sizes(per_page=2)
 #' res <- sizes('raw')
 #' res$url
 #' res$headers
 #' }
 
-sizes <- function(what='parsed', ...)
+sizes <- function(what='parsed', page=1, per_page=25, ...)
 {
-  do_GET(what, path='sizes', parse=if(what=='parsed') TRUE else FALSE, ...)
+  do_GET(what, path='sizes', 
+         query = ct(page=page, per_page=per_page),
+         parse=if(what=='parsed') TRUE else FALSE, ...)
 #   if(what == 'raw'){ res } else {
 #     meta <- res$meta
 #     do.call(rbind.fill, lapply(res$sizes, data.frame, stringsAsFactors = FALSE))
