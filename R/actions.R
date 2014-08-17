3e1153150a1912ad1a7f0f936d2a1710f7aef69a
Scott Chamberlain <myrmecocystus@gmail.com>
2014-08-16 22:54:48 -0700

    completed the  actions function

--- R/actions.R
+++ R/actions.R
@@ -6,29 +6,13 @@
 #' @template params
 #' @examples \dontrun{
 #' actions()
+#' actions(action_id='30841267')
+#' actions(action_id='30219078')
 #' }
 
-actions <- function(id=NULL, what="parsed", ...)
+actions <- function(action_id=NULL, what="parsed", ...)
 {
-#   if(!is.null(droplet)){
-#     if(is.list(droplet)){
-#       if(!is.numeric(droplet$id)) stop("Could not detect a droplet id")
-#     } else {
-#       if(!is.numeric(as.numeric(as.character(droplet)))) stop("Could not detect a droplet id")
-#     }
-#     id <- if(is.numeric(droplet)) droplet else droplet$id
-#   } else { id <- NULL }
-  path <- if(is.null(id)) 'actions' else sprintf('actions/%s', id)
-  tmp <- do_GET(what, FALSE, path, ...)
-  if(what == 'raw'){ tmp } else {
-#     if ("droplet" %in% names(tmp)){
-#       names(tmp) <- "droplets"
-#       ids <- tmp$droplets$id
-#     } else { ids <- sapply(tmp$droplets, "[[", "id") }
-#     list(droplet_ids = ids, droplets = tmp$droplets, event_id=NULL)
-    tmp
-  }
+  path <- if(is.null(action_id)) 'actions' else sprintf('actions/%s', action_id)
+  tmp <- do_GET(what, FALSE, path, parse=TRUE, ...)
+  tmp
 }
-
-
-# curl -X $HTTP_METHOD -H "Authorization: Bearer $TOKEN""https://api.digitalocean.com/v2/$OBJECT"
