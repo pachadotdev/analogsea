15edc95b509efb01dd947fb3d1ecdbf9c9e9f0b6
Scott Chamberlain <myrmecocystus@gmail.com>
2014-08-17 13:08:27 -0700

    changed ... param to config to pass in httr options, still need to change in fxn param definition

--- man-roxygen/params.R
+++ man-roxygen/params.R
@@ -1,4 +1,4 @@
 #' @param what (character) One of list or raw
-#' @param ... Options passed on to httr::GET. Must be named, see examples.
+#' @param config Options passed on to httr::GET. Must be named, see examples.
 #' @param page Page number (Default: 1)
 #' @param per_page Number of items per page (Default: 25)
