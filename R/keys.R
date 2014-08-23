0eac5570fe21c99198eefea7d421bd712cd97ace
Scott Chamberlain <myrmecocystus@gmail.com>
2014-08-23 12:58:50 -0700

    updatd all ssh keys fxns for new v2 api

--- R/keys.R
+++ R/keys.R
@@ -1,18 +1,70 @@
-#' Get metadata on your ssh keys
+#' List your ssh keys, or get a single key
 #'
 #' @export
-#' @param ssh_key_id (numeric) An ssh key id (optional)
+#' @param key (numeric/character) An ssh key id (e.g., 3), or key fingerprint 
+#' (e.g., c8:6f:72:7e:2f:5b:21:38:2d:9a:0b:ef:0f:75:c8:2d). If none given, 
+#' all your keys are listed. (optional)
 #' @template params
 #' @examples \dontrun{
 #' keys()
-#' keys(ssh_key_id=89103)
+#' keys(89103)
+#' keys("6b:2e:f6:be:e7:b4:58:0e:2a:a0:23:7e:16:ac:fc:17")
 #' }
 
-keys <- function(ssh_key_id=NULL, what="parsed", ...)
+keys <- function(key=NULL, what="parsed", page=1, per_page=25, config=NULL)
 {
-  path <- if(is.null(ssh_key_id)) 'ssh_keys' else sprintf('ssh_keys/%s', ssh_key_id)
-  res <- do_GET(what, path=path, ...)
-  if(what == 'raw'){ res } else {
-    res[ !names(res) %in% "status" ]
-  }
+  path <- if(is.null(key)) 'account/keys' else sprintf('account/keys/%s', key)
+  do_GET(what, path=path, parse=FALSE, query = ct(page=page, per_page=per_page), config=config)
+}
+
+#' Create a new ssh key.
+#'
+#' @export
+#' @param name (character) Required. The name to give the new SSH key in your account.
+#' @param public_key (character) Required. A string containing the entire public key.
+#' @template whatconfig
+#' @examples \donttest{
+#' keys_create(name="newkey", "ssh-rsa AB34....")
+#' }
+
+keys_create <- function(name=NULL, public_key=NULL, what="parsed", config=NULL)
+{
+  assert_that(!is.null(name), !is.null(public_key))
+  do_POST(what, path='account/keys', parse=TRUE, 
+                args = ct(name=name, public_key=public_key), config=config)
+}
+
+#' Update one of your ssh keys.
+#'
+#' @export
+#' @param name (character) Required. The name to give the new SSH key in your account.
+#' @param key (numeric/character) An ssh key id (e.g., 3), or key fingerprint 
+#' (e.g., c8:6f:72:7e:2f:5b:21:38:2d:9a:0b:ef:0f:75:c8:2d). If none given, 
+#' all your keys are listed. (optional)
+#' @template whatconfig
+#' @examples \donttest{
+#' keys_rename(name="newkey", key=89103)
+#' }
+
+keys_rename <- function(name=NULL, key=NULL, what="parsed", config=NULL)
+{
+  assert_that(!is.null(name), !is.null(key))
+  do_PUT(what, path=sprintf('account/keys/%s', key), parse=TRUE, args = ct(name=name), config=config)
+}
+
+#' Permanently delete/destroy one of your ssh keys.
+#'
+#' @export
+#' @param key (numeric/character) An ssh key id (e.g., 3), or key fingerprint 
+#' (e.g., c8:6f:72:7e:2f:5b:21:38:2d:9a:0b:ef:0f:75:c8:2d). If none given, 
+#' all your keys are listed. (optional)
+#' @template whatconfig
+#' @examples \donttest{
+#' keys_delete(key=999999)
+#' }
+
+keys_delete <- function(key=NULL, config=NULL)
+{
+  assert_that(!is.null(name), !is.null(key))
+  do_DELETE(path=sprintf('account/keys/%s', key), config=config)
 }
