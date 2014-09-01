4333c4cab28384ddefe50ecf7e3b05cfa801063b
Scott Chamberlain <myrmecocystus@gmail.com>
2014-09-01 07:47:06 -0700

    started kill all droplets fxn, #27

---
+++ R/kill.R
@@ -0,0 +1,29 @@
+#' Kill all droplets.
+#'
+#' @export
+#' @param x A droplet number or the result from a call to \code{droplets()}
+#' @return A message that the droplet(s) has/have been killed.
+#' @examples \dontrun{
+#' droplets_kill() # kill all droplets
+#' droplets_kill(2508542) # kill 1 or more droplets by id numbers
+#' droplets(<droplet id>) %>% droplets_kill # kill all droplets via a call to droplets()
+#' }
+
+droplets_kill <- function(x=NULL, ask=TRUE){
+  ids <- if(is.numeric(x)) { x } else {
+    ids <- droplets()
+    ids$droplets$data$id
+  }
+  if(is.null(ids)) stop("No droplets found :/", call. = FALSE)
+  if(ask){
+    message("Are you sure you want to delete all your droplets? (y/n)")
+    resp <- readline(": ")
+  } else { resp <- 'y' }
+  if(resp %in% c('y','yes','Yes','YES')) invisible(lapply(ids, droplets_delete)) else stop("okay, no worries", call. = FALSE)
+}
+
+# droplets_kill()
+# droplets_kill <- function(x=NULL){
+#   message("Are you sure you want to delete all your droplets? (y/n)")
+#   readline(": ")
+# }
