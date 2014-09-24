5f02ac134db4a1c832ad98bbbb872d26fe5bd45d
hadley <h.wickham@gmail.com>
2014-09-24 11:24:44 -0500

    Re-enable testing & remove old tests

--- tests/testthat/test-domains.R
+++ tests/testthat/test-domains.R
@@ -3,32 +3,6 @@ context("domains")
 
 source("keys.R")
 
-a <- domains()
-b <- domains(what = 'raw')
-
-domnane <- paste0(paste(sample(letters, size = 15, replace = FALSE), collapse = ""), ".info")
-c <- domains_new(name = domnane, ip_address = "123.456.789")
-d <- domains_destroy(domain_id=domnane)
-
-domnane2 <- paste0(paste(sample(letters, size = 15, replace = FALSE), collapse = ""), ".info")
-e <- domains_new(name = domnane2, ip_address = "123.456.123")
-f <- domains_new_record(domain_id=domnane2, record_type="TXT", data="what what")
-g <- domains_edit_record(domain_id=domnane2, record_id=f$record$id, record_type="TXT", data="edited text")
-h <- domains_destroy_record(domain_id=domnane2, record_id=f$record$id)
-
-i <- domains_records(domain_id = domnane2)
-
-test_that("domains returns the correct class", {
-  expect_is(a, "list")
-  expect_is(b, "response")
-  expect_is(c, "list")
-  expect_is(d, "list")
-  expect_is(e, "list")
-  expect_is(f, "list")
-  expect_is(g, "list")
-  expect_is(h, "list")
-})
-
 test_that("incorrect input to what param returns NULL", {
   expect_error(domains_records("asfasd"))
 })
--- tests/testthat/test-droplets.R
+++ tests/testthat/test-droplets.R
@@ -3,21 +3,6 @@ context("droplets")
 
 source("keys.R")
 
-a <- droplets()
-b <- droplets(what = 'raw')
-
-test_that("droplets returns the correct dimensions", {
-  expect_equal(length(a), 3)
-  expect_equal(names(a), c('droplet_ids','droplets','event_id'))
-
-  expect_equal(length(b), 8)
-})
-
-test_that("droplets returns the correct class", {
-  expect_is(a, "list")
-  expect_is(b, "response")
-})
-
 test_that("httr curl options work", {
   library("httr")
   expect_error(droplets(config=timeout(seconds = 0.001)))
