3ae23262c169d8da04bbfba5a7946597bbe1a75d
Scott Chamberlain <myrmecocystus@gmail.com>
2014-09-27 10:44:36 -0400

    renamed params template to pages

---
+++ man-roxygen/pages.R
@@ -0,0 +1,2 @@
+#' @param page Page number (Default: 1)
+#' @param per_page Number of items per page (Default: 25)
--- man-roxygen/params.R
+++
@@ -1,4 +0,0 @@
-#' @param what (character) One of 'parsed' or 'raw'.
-#' @param page Page number (Default: 1)
-#' @param per_page Number of items per page (Default: 25)
-#' @param config Options passed on to httr::GET. Must be named, see examples.
