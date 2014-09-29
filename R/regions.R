6ed914ce4519ec6e453f962aba0cad38c00b5bff
hadley <h.wickham@gmail.com>
2014-09-29 07:23:19 -0500

    Parse regions as data frame. #43

--- R/regions.R
+++ R/regions.R
@@ -5,8 +5,19 @@
 #' @param ... Options passed on to httr::GET. Must be named, see examples.
 #' @examples \dontrun{
 #' regions()
-#' regions(per_page=2)
 #' }
-regions <- function(page=1, per_page=25, ...) {
-  do_GET('regions', query = list(page = page, per_page  =per_page), ...)
+regions <- function(page = 1, per_page = 25, ...) {
+  res <- do_GET('regions', query = list(page = page, per_page = per_page), ...)
+  regions <- res$regions
+  
+  data.frame(
+    slug = pluck(regions, "slug", character(1)),
+    name = pluck(regions, "name", character(1)),
+    sizes = vapply(regions, function(x) paste0(x$sizes, collapse = ", "),
+      character(1)),
+    available = pluck(regions, "available", logical(1)),
+    features = vapply(regions, function(x) paste0(x$features, collapse = ", "),
+      character(1)),
+    stringsAsFactors = FALSE
+  ) 
 }
