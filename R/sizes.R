0995f7c0a89d914d7fece6b097ff0c1e7262e0e9
hadley <h.wickham@gmail.com>
2014-09-29 07:27:42 -0500

    Parse sizes as data frame. #43

--- R/sizes.R
+++ R/sizes.R
@@ -6,11 +6,22 @@
 #' @param ... Options passed on to httr::GET. Must be named, see examples.
 #' @examples \dontrun{
 #' sizes()
-#' sizes(per_page=2)
-#' res$url
-#' res$headers
 #' }
-
-sizes <- function(page=1, per_page=25, ...) {
-  do_GET('sizes', query = list(page = page, per_page = per_page))
+sizes <- function(page = 1, per_page = 25, ...) {
+  res <- do_GET('sizes', query = list(page = page, per_page = per_page), ...)
+  sizes <- res$sizes
+  
+  data.frame(
+    slug = pluck(sizes, "slug", character(1)),
+    memory = pluck(sizes, "memory", numeric(1)),
+    vcpus = pluck(sizes, "vcpus", numeric(1)),
+    disk = pluck(sizes, "disk", numeric(1)),
+    transfer = pluck(sizes, "transfer", numeric(1)),
+    price_monthly = pluck(sizes, "price_monthly", numeric(1)),
+    price_hourly = pluck(sizes, "price_hourly", numeric(1)),
+    region = vapply(sizes, function(x) paste0(sort(unlist(x$regions)), collapse = ", "),
+      character(1)),
+    stringsAsFactors = FALSE
+  )
+  
 }
