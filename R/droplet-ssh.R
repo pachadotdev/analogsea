ced1f4c4cdd5207f2bf75b62957a4758e0f5bee8
hadley <h.wickham@gmail.com>
2014-09-30 17:03:19 -0500

    Add upload and download functions

--- R/droplet-ssh.R
+++ R/droplet-ssh.R
@@ -1,12 +1,14 @@
-#' Remotely execute ssh code on a droplet.
+#' Remotely execute ssh code, upload & download files.
 #' 
-#' Assumes that you have ssh installed, and password-less login set up
+#' Assumes that you have ssh & scp installed, and password-less login set up
 #' on the droplet.
 #' 
 #' @param droplet  A droplet, or something that can be coerced to a droplet by
 #'   \code{\link{as.droplet}}.
 #' @param cmd Shell command to run
-#' @param user User
+#' @param user User name. Defaults to "root".
+#' @param local,remote Local and remote paths.
+#' @param verbose If TRUE, will print command before executing it.
 #' @return On success, the droplet (invisibly). On failure, throws an 
 #'   error.
 #' @examples
@@ -22,21 +24,47 @@
 #' # Install R
 #' d %>%
 #'   droplet_ssh("apt-get install r-base-core r-base-dev --yes --force-yes")
+#'   
+#' # Upload and download files -------------------------------------------------
+#'    
+#' tmp <- tempfile()
+#' saveRDS(mtcars, tmp)
+#' d %>% droplet_upload(tmp, "mtcars.rds")
+#' 
+#' tmp2 <- tempfile()
+#' d %>% droplet_download("mtcars.rds", tmp2)
+#' mtcars2 <- readRDS(tmp2)
+#' 
+#' stopifnot(all.equal(mtcars, mtcars2))
 #' }
 droplet_ssh <- function(droplet, cmd, user = "root", verbose = FALSE) {
-  droplet <- as.droplet(droplet)
+  droplet <- as.droplet(droplet)  
   
   cmd <- sprintf("ssh -o StrictHostKeyChecking=no %s@%s %s", user, 
     droplet_ip(droplet), shQuote(cmd))
-  
-  mssg(verbose, cmd)
-  status <- system(cmd)
-  if (status != 0) {
-    stop("ssh failed\n", cmd, call. = FALSE)
-  }
-  
-  invisible(droplet)
+  do_system(droplet, cmd, verbose = verbose)
 }
+
+#' @export
+#' @rdname droplet_ssh
+droplet_upload <- function(droplet, local, remote, user = "root", verbose = FALSE) {
+  droplet <- as.droplet(droplet)  
+  
+  cmd <- sprintf("scp -o StrictHostKeyChecking=no %s %s@%s:~/%s", 
+    local, user, droplet_ip(droplet), remote)
+  do_system(droplet, cmd, verbose = verbose)  
+}
+
+#' @export
+#' @rdname droplet_ssh
+droplet_download <- function(droplet, remote, local, user = "root", verbose = FALSE) {
+  droplet <- as.droplet(droplet)  
+  
+  cmd <- sprintf("scp -o StrictHostKeyChecking=no %s@%s:~/%s %s", 
+    user, droplet_ip(droplet), remote, local)
+  do_system(droplet, cmd, verbose = verbose)  
+}
+
 
 droplet_ip <- function(x) {
   v4 <- x$network$v4
@@ -46,4 +74,15 @@ droplet_ip <- function(x) {
   }
   
   v4[[1]]$ip_address
+}
+
+
+do_system <- function(droplet, cmd, verbose = FALSE) {
+  mssg(verbose, cmd)
+  status <- system(cmd)
+  if (status != 0) {
+    stop("ssh failed\n", cmd, call. = FALSE)
+  }
+  
+  invisible(droplet)  
 }
