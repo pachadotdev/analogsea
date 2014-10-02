ae7f5fdaf7486a7b48ab61a7d79c0ca55e66834b
hadley <h.wickham@gmail.com>
2014-10-02 17:28:27 -0500

    Remoter -> droplet_execute. And use new helpers. Assumes droplet exists to simplify code

--- R/remoter.R
+++ R/remoter.R
@@ -1,94 +1,50 @@
-#' Execute code remotely on Digital Ocean in a single command.
+#' Execute R code on a droplet.
 #'
-#' See Details.
+#' Assumes that the droplet has R installed.
 #'
-#' @param x Code to excute on Digital Ocean droplet.
+#' @param droplet A droplet, or object that can be coerced to a droplet
+#'   by \code{\link{as.droplet}}.
+#' @param code Code to excute on Digital Ocean droplet.
 #' @param verbose (logical) Print messages
-#' @details
-#' This function (when complete, that is):
-#' \itemize{
-#'  \item Spin up a new droplet, or use an existing one.
-#'  \item Parse your code input to parameter \code{x}
-#'  \item Send code to droplet
-#'  \item Execute code on droplet
-#'  \item Return code to the session or directory when complete
-#'  \item Destory droplet
-#' }
-#' @examples \dontrun{
-#' # If you already have a droplet
-#' (d <- droplets()[[1]])
-#' path <- remoter({
-#'   install.packages("ggplot2")
-#'   library('ggplot2')
-#'   carsplot <- qplot(mpg, wt, data=mtcars)
-#'   df <- diamonds[1:5000,]
-#'   diaomondsplot <- qplot(cut, carat, data=df)
-#'   numbers <- runif(100)
-#' }, droplet = d)
-#' remoter_load(path)
+#' @examples \donttest{
+#' d <- droplet_new() %>% 
+#'   debian_add_swap() %>%
+#'   debian_install_r()
 #' 
-#' # With a new droplet
-#' path <- remoter({
-#'   numbers <- runif(100)
-#' }, droplets()[[3]])
-#' remoter_load(path)
+#' results <- d %>% droplet_execute({
+#'   x <- letters
+#'   numbers <- runif(1000)
+#' })
+#' 
+#' droplet_delete(d)
 #' }
-
-remoter <- function(code, droplet=NULL, verbose=TRUE, savepath=NULL){
+droplet_execute <- function(droplet, code, verbose=TRUE, savepath=NULL) {
+  browser()
+  droplet <- as.droplet(droplet)
   
-  # spin up new droplet, collect info
-  if(is.null(droplet)) {
-    droplet <- droplet_new()
-    droplet <- droplet_wait(droplet)
+  code <- substitute(code)
+  # Remove surrounding `{`
+  if (identical(code[[1]], quote(`{`))) {
+    code <- as.list(code[-1])
   } else {
-    droplet <- as.droplet(droplet)
+    code <- list(code)
   }
-
-  id <- droplet$id
-  ip <- droplet_ip(droplet)
+  text <- unlist(lapply(code, deparse))
   
-  # check if R is installed, and if not, install R
-  do_swap(TRUE, ip, swap_string, verbose)
-  r_installed(ip, r_string, verbose)
-
-  # deparse input code, write to file
-  code_string <- parse_code(code)
-  writefile("remoter_code.R", code_string)
-
-  # send and run code on droplet
-  mssg(verbose, "Sending R code to DO Droplet...")
-  scp('remoter_code.R', ip)
-  mssg(verbose, "Running R code on DO Droplet...")
-  writefile('remoter_run.sh', sprintf(run_r, 'remoter_code.R'))
-  scp_ssh('remoter_run.sh', ip, verbose = verbose)
-
-  # Retrieve results from droplet
-  mssg(verbose, "Receiving results from DO Droplet...")
-  savepath <- if(is.null(savepath)) tempdir() else savepath
-  scp_get(file = 'output.RData', ip, path = savepath)
-
-  # Delete the droplet
-  answer <- readline("Delete droplet? (y/n) ")
-  answer <- match.arg(answer, c('yes','no'))
-  if(answer=="yes"){
-    droplet_delete(droplet)
-    message(sprintf("Droplet %s deleted", id))
-  } else { message("droplet still running") }
+  mssg(verbose, "Uploading R code to droplet...")
+  tmp <- tempfile()
+  writeLines(text, tmp)  
+  droplet_upload(droplet, tmp, "remote.R")
   
-  return( savepath )
-}
+  mssg(verbose, "Running R code...")
+  droplet_ssh(droplet, "Rscript --save remote.R")
 
-remoter_load <- function(x){
-  path <- file.path(x, 'output.RData')
-  assert_that(file.exists(path))
-  load(path, envir = .GlobalEnv, verbose = TRUE)
+  mssg(verbose, "Downloading results...")
+  tmp <- tempfile()
+  droplet_download(droplet, ".Rdata", tmp)
+  
+  e <- new.env(parent = emptyenv())
+  load(tmp, envir = e)
+  
+  as.list(e)
 }
-
-parse_code <- function(x){
-  tmp <- deparse(substitute(x))
-  tmp <- gsub("\\s+", "", tmp)
-  tmp <- tmp[-c(1,length(tmp))]
-  paste(tmp, collapse = "\n")
-}
-
-run_r <- 'R -e \'source(\"%s\"); save.image(\"~/output.RData\")\''
