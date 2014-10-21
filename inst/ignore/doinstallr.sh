ccef899205b305b0518248bba4f433a2a257ee8a
hadley <h.wickham@gmail.com>
2014-10-21 09:36:30 -0500

    Remove old install scripts

--- inst/ignore/doinstallr.sh
+++
@@ -1,4 +0,0 @@
-sudo echo "deb http://cran.rstudio.com/bin/linux/ubuntu trusty/" >> /etc/apt/sources.list
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
-sudo apt-get update --yes --force-yes
-sudo apt-get install r-base-dev --yes --force-yes
