6346229959a00582228a04484c575b8b878539f0
Scott Chamberlain <myrmecocystus@gmail.com>
2014-12-03 08:42:03 -0800

    added account fxn to wrap accounts endpoint closes #90

---
+++ R/account.R
@@ -0,0 +1,18 @@
+#' Get account information
+#'
+#' @export
+#' @param ... Options passed down to \code{\link[httr]{GET}}
+#' @inheritParams droplets
+#' @examples \dontrun{
+#' account()
+#' }
+account <- function(...) structure(do_GET("account", list(), ...), class="account")
+
+#' @export
+print.account <- function(x, ...) {
+  cat("<account> ", sep = "\n")
+  cat("  E-mail:          ", x$account$email, "\n")
+  cat("  E-mail verified: ", x$account$email_verified, "\n")
+  cat("  UUID:            ", x$account$uuid, "\n")
+  cat("  Droplet limit:   ", x$account$droplet_limit)
+}
