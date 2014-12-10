8df42ae0717f243a0632cca85c394b26cf8287a3
Scott Chamberlain <myrmecocystus@gmail.com>
2014-12-10 10:28:14 -0800

    renamed reuse to droplet_reuse, #94

--- R/reuse.R
+++ R/reuse.R
@@ -5,26 +5,22 @@
 #' @param name A name that could be a droplet or image name
 #' @param ... Named options passed on to \code{\link{droplet_create}}.
 #' @examples \dontrun{
-#' reuse(name = 'BeguiledAmmonia') # matches droplet that exists
-#' reuse(name = 'hadleyverse1', size = "1gb") # matching image that exists
-#' reuse(name = 'tablesandchairs') # no matching droplet or image
+#' droplet_reuse(name = 'BeguiledAmmonia') # matches droplet that exists
+#' droplet_reuse(name = 'hadleyverse1', size = "1gb") # matching image that exists
+#' droplet_reuse(name = 'tablesandchairs') # no matching droplet or image
 #' }
 
-reuse <- function(name, ...) {
+droplet_reuse <- function(name, ...) {
   drops <- droplets()
   imgs <- images(FALSE)
   if (name %in% names(drops)) {
-    message(same)
+    message("droplet of same name already exists")
     drops[[name]]
   } else if (name %in% names(imgs)) {
-    message(fromimg)
+    message("image of same name, creating droplet...")
     droplet_create(name, image = imgs[[name]]$id, ...)
   } else {
-    message(freshnew)
+    message("no matched droplets or images, creating new droplet...")
     droplet_create(name, ...)
   }
 }
-
-same <- "droplet of same name already exists"
-fromimg <- "image of same name, creating droplet..."
-freshnew <- "no matched droplets or images, creating new droplet..."
