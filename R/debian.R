8a15be4b9857b7c76b45b80bfa10459a4972b274
Scott Chamberlain <myrmecocystus@gmail.com>
2015-03-12 16:59:08 -0700

    changed debian to ubuntu for getting shiny

--- R/debian.R
+++ R/debian.R
@@ -1,5 +1,5 @@
 #' Helpers for managing an debian droplets.
-#' 
+#'
 #' @param droplet A droplet, or object that can be coerced to a droplet
 #'   by \code{\link{as.droplet}}.
 #' @name debian
@@ -8,10 +8,10 @@
 #' d <- droplet_create()
 #' d %>% debian_add_swap()
 #' d %>% debian_apt_get_update()
-#' 
+#'
 #' d %>% debian_install_r()
 #' d %>% debian_install_rstudio()
-#' 
+#'
 #' # Install libcurl, then build RCurl from source
 #' d %>% debian_apt_get_install("libcurl4-openssl-dev")
 #' d %>% install_r_package("RCurl")
@@ -22,7 +22,7 @@ NULL
 #' @rdname debian
 #' @export
 debian_add_swap <- function(droplet) {
-  droplet_ssh(droplet, 
+  droplet_ssh(droplet,
     "fallocate -l 4G /swapfile",
     "chmod 600 /swapfile",
     "mkswap /swapfile",
@@ -43,7 +43,7 @@ debian_install_r <- function(droplet) {
 #' @param password Default password for Rstudio.
 #' @param version Version of rstudio to install.
 #' @export
-debian_install_rstudio <- function(droplet, user = "rstudio", password = "rstudio", 
+debian_install_rstudio <- function(droplet, user = "rstudio", password = "rstudio",
                                    version = "0.98.1062") {
   droplet %>%
     debian_apt_get_install("gdebi-core", "libapparmor1") %>%
@@ -52,7 +52,7 @@ debian_install_rstudio <- function(droplet, user = "rstudio", password = "rstudi
       sprintf("sudo gdebi rstudio-server-%s-amd64.deb --non-interactive", version),
       sprintf('adduser %s --disabled-password --gecos ""', user),
       sprintf('echo "%s:%s" | chpasswd', user, password)
-    )   
+    )
 }
 
 #' @rdname debian
@@ -64,7 +64,7 @@ debian_install_shiny <- function(droplet, version = "1.2.1.362") {
     install_r_package("rmarkdown") %>%
     debian_apt_get_install("gdebi-core") %>%
     droplet_ssh(
-      sprintf("wget http://download3.rstudio.org/debian-12.04/x86_64/shiny-server-%s-amd64.deb", version),
+      sprintf("wget http://download3.rstudio.org/ubuntu-12.04/x86_64/shiny-server-%s-amd64.deb", version),
       sprintf("sudo gdebi shiny-server-%s-amd64.deb --non-interactive", version)
     )
 }
@@ -86,7 +86,7 @@ debian_install_opencpu <- function(droplet) {
 #' @rdname debian
 #' @export
 debian_apt_get_update <- function(droplet) {
-  droplet_ssh(droplet, 
+  droplet_ssh(droplet,
     "sudo apt-get update -qq",
     "sudo apt-get upgrade -y"
   )
@@ -96,7 +96,7 @@ debian_apt_get_update <- function(droplet) {
 #' @export
 #' @param ... Arguments to apt-get install.
 debian_apt_get_install <- function(droplet, ...) {
-  droplet_ssh(droplet, 
+  droplet_ssh(droplet,
     paste0("sudo apt-get install -y --force-yes ", paste(..., collapse = " "))
   )
 }
