c7412279db0aa92c859b1c7306d7a03a448d2a52
Scott Chamberlain <myrmecocystus@gmail.com>
2015-08-07 13:54:13 -0700

    added status and status_message to print method for account() fix #103

--- R/account.R
+++ R/account.R
@@ -6,7 +6,7 @@
 #' @examples \dontrun{
 #' account()
 #' }
-account <- function(...) structure(do_GET("account", list(), ...), class="account")
+account <- function(...) structure(do_GET("account", list(), ...), class = "account")
 
 #' @export
 print.account <- function(x, ...) {
@@ -14,5 +14,7 @@ print.account <- function(x, ...) {
   cat("  E-mail:          ", x$account$email, "\n")
   cat("  E-mail verified: ", x$account$email_verified, "\n")
   cat("  UUID:            ", x$account$uuid, "\n")
-  cat("  Droplet limit:   ", x$account$droplet_limit)
+  cat("  Droplet limit:   ", x$account$droplet_limit, "\n")
+  cat("  Status:          ", x$account$status, "\n")
+  cat("  Status message:  ", x$account$status_message)
 }
