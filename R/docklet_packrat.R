47a2296bb00ceda3fa2cabbde0432eabe974bb99
Scott Chamberlain <myrmecocystus@gmail.com>
2015-10-01 22:51:22 -0700

    dont export docklet_packrat anymore

--- R/docklet_packrat.R
+++ R/docklet_packrat.R
@@ -2,7 +2,6 @@
 #'
 #' Currently a work in progress.
 #'
-#' @export
 #' @param droplet A docklet, created with \code{\link{docklet_create}}.
 #'   Must have docker installed.
 #' @param project Path to a rstudio + packrat project.
@@ -13,26 +12,26 @@
 #' d <- docklet_create()
 #' d %>% docklet_packrat(".")
 #' }
-docklet_packrat <- function(droplet, project, user = 'rstudio', 
+docklet_packrat <- function(droplet, project, user = 'rstudio',
                             password = 'rstudio', verbose = TRUE) {
   if (!requireNamespace("packrat", quietly = TRUE)) {
     stop("Please install packrat", call. = FALSE)
   }
-  
+
   droplet <- as.droplet(droplet)
-  
+
   if (!file.exists(project) || !file.info(project)$isdir) {
     stop("Project must exist and be a directory", call. = FALSE)
   }
   name <- dirname(project)
-  
+
   mssg(verbose, "Bundling project and uploading...")
   bundle <- packrat::bundle(project, overwrite = TRUE)
   droplet_upload(droplet, bundle, paste0(name, ".tar.gz"))
-  
+
   # Tell remote (server? container?) to unbundle
   mssg(verbose, "Unbundling and installing...")
-  
+
   # Launch rstudio:
   # * Need to randomise port (or select unused)
   # * Need to attach volume
