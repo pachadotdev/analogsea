0956d2aaa5132956bc84e7b6830afb9369e8bd7c
Scott Chamberlain <myrmecocystus@gmail.com>
2015-10-01 19:23:46 -0700

    added clarification to droplet_reuse as to what happens internally in addition, changed the images() call internally to private=TRUE to only get private images makes no sense, i think, for user to want to reuse a public image which they can easily do via droplet_create()

--- R/droplet_reuse.R
+++ R/droplet_reuse.R
@@ -4,6 +4,9 @@
 #' @return A droplet
 #' @param name A name that could be a droplet or image name
 #' @param ... Named options passed on to \code{\link{droplet_create}}.
+#' @details Internally, we call the \code{\link{droplets}} and
+#' \code{\link{images}} (with \code{private = TRUE}) to get list of your
+#' droplets and images - and we check against those.
 #' @examples \dontrun{
 #' droplet_reuse(name = 'BeguiledAmmonia') # matches droplet that exists
 #' droplet_reuse(name = 'hadleyverse1', size = "1gb") # matching image that exists
@@ -12,7 +15,7 @@
 
 droplet_reuse <- function(name, ...) {
   drops <- droplets()
-  imgs <- images(FALSE)
+  imgs <- images(TRUE)
   if (name %in% names(drops)) {
     message("droplet of same name already exists")
     drops[[name]]
