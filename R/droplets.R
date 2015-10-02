c978184e1dd233da5a2a86749048654061616023
Scott Chamberlain <myrmecocystus@gmail.com>
2015-10-01 19:24:43 -0700

    simplify list naming using setNames in droplets() fxn

--- R/droplets.R
+++ R/droplets.R
@@ -1,7 +1,7 @@
 #' List all available droplets.
 #'
 #' @export
-#' @param ... Additional arguments passed down to low-level API function 
+#' @param ... Additional arguments passed down to low-level API function
 #'   (\code{do_*})
 #' @param page Page to return. Default: 1.
 #' @param per_page Number of results per page. Default: 25.
@@ -11,6 +11,5 @@
 droplets <- function(..., page = 1, per_page = 25) {
   res <- do_GET("droplets", query = list(page = page, per_page = per_page), ...)
   droplets <- lapply(res$droplets, structure, class = "droplet")
-  names(droplets) <- vapply(res$droplets, function(x) x$name, character(1))
-  droplets
+  setNames(droplets, vapply(res$droplets, function(x) x$name, character(1)))
 }
