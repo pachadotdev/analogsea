4ad99bcfc6bbcb8e9cbc145e475e64c49687a42c
Scott Chamberlain <myrmecocystus@gmail.com>
2017-04-27 14:44:02 -0700

    move words/adject/nouns into pkg in r files instead of in data/, #125

--- R/onload.R
+++ R/onload.R
@@ -5,10 +5,5 @@
   )
   toset <- !(names(op.do) %in% names(op))
   if (any(toset)) options(op.do[toset])
-
-  utils::data("words")
-  utils::data("adjectives")
-  utils::data("nouns")
-
   invisible()
 }
