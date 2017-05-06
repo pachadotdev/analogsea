38d24a68d7a2c01a2a390b71f182de757dcf9da5
Scott Chamberlain <myrmecocystus@gmail.com>
2017-05-05 17:41:10 -0700

    fix #127 - in debian_add_swap make swap permanent - bump dev ver

--- R/debian.R
+++ R/debian.R
@@ -26,7 +26,8 @@ debian_add_swap <- function(droplet) {
     "fallocate -l 4G /swapfile",
     "chmod 600 /swapfile",
     "mkswap /swapfile",
-    "sudo swapon /swapfile"
+    "sudo swapon /swapfile",
+    "sudo echo \"/swapfile   none    swap    sw    0   0\" >> /etc/fstab"
   )
 }
 
