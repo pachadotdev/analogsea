ceed414ae711c978d2accf0b89895c907657f9a9
Eric Nantz <thercast@gmail.com>
2017-10-11 22:41:47 -0400

    tidy long lines in spaces tests

--- tests/testthat/test-space.R
+++ tests/testthat/test-space.R
@@ -9,8 +9,10 @@ test_that("key checks fail when not defined in environment", {
   skip_on_cran()
 
   Sys.unsetenv("DO_SPACES_ACCESS_KEY")
-  expect_error(check_space_access(spaces_key = NULL), "Need a digital ocean spaces access key defined in your session")
+  expect_error(check_space_access(spaces_key = NULL),
+               "Need a digital ocean spaces access key defined in your session")
 
   Sys.unsetenv("DO_SPACES_SECRET_KEY")
-  expect_error(check_space_secret(spaces_secret = NULL), "Need a digital ocean spaces secret key defined in your session")
+  expect_error(check_space_secret(spaces_secret = NULL),
+               "Need a digital ocean spaces secret key defined in your session")
 })
